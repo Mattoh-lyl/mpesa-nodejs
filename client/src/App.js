04e5b4b0d656daec281e1f32917d037cc0df3efd
MOZES <mwauramnjoki@gmail.com>
2022-04-14 14:45:38 +0300

    mpesa

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ setserver('! Enter a valid amount')
    }
    else{
      setserver("")
-   await axios.post('https://trympesa.herokuapp.com//stk/push', {pno,amount}).then((response)=>{
+   await axios.post('https://trympesa.herokuapp.com/stk/push', {pno,amount}).then((response)=>{
      console.log(response)
       if (response.data.message==="Bad Request - Invalid PhoneNumber")
      {
