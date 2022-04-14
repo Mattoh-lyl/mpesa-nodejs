3d0d514fd1898cdbd8235a1b8e996f912aa72350
MOZES <mwauramnjoki@gmail.com>
2022-04-14 14:23:22 +0300

    mpesa

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended: true}))
 
 if(process.env.NODE_ENV==="production"){
     app.use(express.static('react-demo-app/build'))
-    app.get('*',(req,res)=>{
+    app.get('/',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client/build','index.html'));
     })
 }
